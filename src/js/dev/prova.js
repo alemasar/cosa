6c64c54707a776225c426e897ff28d484b340113
aleixmasague <aleixmasague@dorna.com>
2018-02-05 11:03:10 +0100

    Third version

--- src/js/dev/prova.js
+++ src/js/dev/prova.js
@@ -1,20 +1,43 @@
-import {test} from './prova1/prova';
+import * as classes from './prova1/src/methods';
 
-const module = new Proxy(test, {
-    get: (Target, name, receiver) => {
-        const t = new Target();
-console.log(t)
-        return t[name];
-    }
-});
+const protos = [];
 
 
+const keys = Object.keys(classes);
+keys.forEach((k) => {
+    protos.push(new classes[k]());
+})
 
 
+console.log(protos)
+const moduleCreate = (...prototypes) => {
+    const overrides = {}
+
+    return new Proxy(prototypes[0][0], {  
+        set: (target, property, value) => {
+            overrides[property] = value
+
+            return true;
+        },
+
+        get: (target, property) => {
+            const prototype = [overrides].
+                concat(protos).
+                find((proto) => property in proto);
+
+            if (prototype) {
+                return prototype[property]
+            }
+
+            return false;
+        }
+    })
+}
+const module = moduleCreate(protos)
 
 
 document.addEventListener('DOMContentLoaded', (e) => {
-    console.log(module.yoqueser(1, 2))
+    console.log(module)
     console.log(module.bar(2, 1))
 });
 
